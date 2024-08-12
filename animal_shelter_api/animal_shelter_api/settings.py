049eddc5ca80d144331987e2e4fbbfdf325f5ead
Ismael Vargas <ismael.ubk@gmail.com>
2024-08-12 00:01:06 -0500

    feat: reset jwt access lifetime

--- animal_shelter_api/animal_shelter_api/settings.py
+++ animal_shelter_api/animal_shelter_api/settings.py
@@ -166,7 +166,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=225),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
