ef53e2e34bb5609f5745ebf176977b5f4953b59a
Ismael Vargas <ismael.ubk@gmail.com>
2024-08-11 19:43:03 -0500

    deploy: fix configure build file

--- animal_shelter_api/build_files.sh
+++ animal_shelter_api/build_files.sh
@@ -1,3 +1,4 @@
-pip install -r requirements.txt
-
-python manage.py collectstatic --noinput
+python3 -m pip install -r requirements.txt
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+python3 python manage.py collectstatic --noinput
