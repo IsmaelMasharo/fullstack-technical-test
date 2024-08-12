77eb1d2713f509b595bda8a75c1e634dd4fb2347
Ismael Vargas <ismael.ubk@gmail.com>
2024-08-11 21:26:46 -0500

    deploy: configure client deployment

--- animal_shelter_client/src/api/axios.ts
+++ animal_shelter_client/src/api/axios.ts
@@ -1,5 +1,8 @@
 import axios from "axios"
-const BASE_URL = "http://localhost:8000"
+const BASE_URL =
+  process.env.ENVIRONMENT === "local"
+    ? process.env.REACT_LOCAL_API
+    : process.env.REACT_PROD_API
 
 export default axios.create({
   baseURL: BASE_URL,
