b0eb38d91db57bea8cffc68fef688d99c6b0f9ec
Ismael Vargas <ismael.ubk@gmail.com>
2024-08-11 23:22:56 -0500

    fix: configure backend env variables

--- animal_shelter_client/src/api/axios.ts
+++ animal_shelter_client/src/api/axios.ts
@@ -4,7 +4,6 @@ const BASE_URL =
     ? import.meta.env.VITE_PROD_API
     : import.meta.env.VITE_LOCAL_API
 
-console.log({ BASE_URL, env: import.meta.env })
 export default axios.create({
   baseURL: BASE_URL,
 })
